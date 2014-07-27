cf59e9bb53da8c3e066cc9f2dd5d130756e5d8a3
efleurisson <eric.fleurisson@gmail.com>
2014-07-27 17:50:59 +0200

    Add some metrics

--- src/main/java/test/metrics/BusinessProcessor.java
+++ src/main/java/test/metrics/BusinessProcessor.java
@@ -1,12 +1,45 @@
 package test.metrics;
 
-import org.springframework.stereotype.Component;
+import java.io.File;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
+import com.codahale.metrics.ConsoleReporter;
+import com.codahale.metrics.Counter;
+import com.codahale.metrics.CsvReporter;
+import com.codahale.metrics.Meter;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Timer;
 
 public class BusinessProcessor {
-	
-	
-	public void process() {
-		System.out.println("Processing request");
+
+	final static MetricRegistry metrics = new MetricRegistry();
+
+	static {
+		final ConsoleReporter reporter = ConsoleReporter.forRegistry(metrics).convertRatesTo(TimeUnit.SECONDS)
+				.convertDurationsTo(TimeUnit.MILLISECONDS).build();
+//		reporter.start(20, TimeUnit.SECONDS);
+
+		final CsvReporter csvreporter = CsvReporter.forRegistry(metrics).formatFor(Locale.FRENCH).convertRatesTo(TimeUnit.SECONDS)
+				.convertDurationsTo(TimeUnit.MILLISECONDS).build(new File("\\\\D:\\Travail\\Projet\\MetricsTest\\metrics"));
+		csvreporter.start(1, TimeUnit.SECONDS);
+	}
+
+	final Meter requests = metrics.meter(MetricRegistry.name(BusinessProcessor.class, "requests"));
+	private final Counter requestNb = metrics.counter(MetricRegistry.name(BusinessProcessor.class, "requestNumber"));
+	private final Timer responses = metrics.timer(MetricRegistry.name(BusinessProcessor.class, "responses"));
+
+	public void process() throws InterruptedException {
+		requestNb.inc();
+		requests.mark();
+		final Timer.Context context = responses.time();
+
+		double random = Math.random();
+		double d = random * 2000;
+		System.out.println("Processing request, sleeping " + d + " ms");
+		Thread.sleep((long) d);
+
+		context.stop();
 	}
 
 }
--- src/main/java/test/metrics/SimpleServlet.java
+++ src/main/java/test/metrics/SimpleServlet.java
@@ -7,21 +7,24 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 public class SimpleServlet extends HttpServlet {
 
 	private static final long serialVersionUID = -977083570839354632L;
 	
 //	@Autowired
-	private BusinessProcessor processor;
+	private BusinessProcessor processor = new BusinessProcessor();
 
 	public SimpleServlet() {
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-//		processor.process();
+		try {
+			processor.process();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+			throw new ServletException(e.getMessage());
+		}
 		
 		response.setContentType("text/html");
 		response.setStatus(HttpServletResponse.SC_OK);
